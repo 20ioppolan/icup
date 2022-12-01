fc93c50d024b8dd3f3fe25e3415ccd8a28cbf038
Anthony Ioppolo <20ioppolan@gmail.com>
2022-12-01 16:41:44 -0500

    target generation

--- iGOp/generatetargets.py
+++ iGOp/generatetargets.py
@@ -5,21 +5,24 @@ import json
 import os
 
 def main():
-    targets = dict()
     teams = input("Enter number of teams or \"wipe\" to delete current: ")
     if teams == "wipe":
         os.system("rm targets.json")
     if not teams.isdigit():
         print("Bruh")
     else:
-        for x in range(1,int(teams)+1):
-            targets[f"Team{x}Box4"] = f"10.{x}.1.4" 
-            targets[f"Team{x}Box5"] = f"10.{x}.1.5"
+        with open ("targets.txt", "w") as f:
+            for x in range(1,int(teams)+1):
+                f.write(f"10.{x}.1.10\n")
+                f.write(f"10.{x}.1.40\n")
+                f.write(f"10.{x}.2.2\n")
+                f.write(f"10.{x}.2.3\n")
+                f.write(f"10.{x}.2.4\n")        
 
-        json_object = json.dumps(targets, indent=4)
+        # json_object = json.dumps(targets, indent=4)
 
-        with open("targets.json", "w") as outfile:
-            outfile.write(json_object)
+        # with open("targets.json", "w") as outfile:
+        #     outfile.write(json_object)
     
 
 

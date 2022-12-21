0c196b987adabe874a2f696e01252cb38f910a91
Anthony Ioppolo <20ioppolan@gmail.com>
2022-12-21 16:19:58 -0500

    have simple functions cleaned, gonna add some crazy target generation now

--- iGOp2/server/server.go
+++ iGOp2/server/server.go
@@ -1,1 +1,99 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var clients = make(map[int]string)
+
+var ID int = 0
+
+func print_title() {
+	fmt.Println(".__                          ___             .__.__    ___")
+	fmt.Println("|__| ____  __ ________      /  /   _______  _|__|  |   \\  \\")
+	fmt.Println("|  _/ ___\\|  |  \\____ \\    /  /  _/ __ \\  \\/ |  |  |    \\  \\")
+	fmt.Println("|  \\  \\___|  |  |  |_> >  (  (   \\  ___/\\   /|  |  |__   )  )")
+	fmt.Println("|__|\\___  |____/|   __/    \\  \\   \\___  >\\_/ |__|____/  /  /")
+	fmt.Println("        \\/      |__|        \\__\\      \\/               /__/")
+	fmt.Println("We are so back.")
+	fmt.Println("Type \"help\" for list of commands.")
+
+}
+
+func AddClient(ip string) {
+	clients[ID] = ip
+	ID++
+}
+
+func RemoveClient(id int) {
+	_, ok := clients[id]
+	if !ok {
+		fmt.Println("Invalid ID")
+	}
+	delete(clients, id)
+}
+
+func RemoveAllClients() {
+	for k := range clients {
+		RemoveClient(k)
+	}
+}
+
+func ShowClients() {
+	for id, ip := range clients {
+		fmt.Printf("ID: %d, IP: %s\n", id, ip)
+	}
+}
+
+func CheckTwoStrings(words []string) bool {
+	if len(words) != 2 {
+		fmt.Println("Invalid input")
+		return false
+	}
+	return true
+}
+
+func main() {
+	print_title()
+	for {
+		consoleReader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		input, err := consoleReader.ReadString('\n')
+		input = strings.TrimRight(input, "\r\n")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		tokens := strings.Split(input, " ")
+
+		switch tokens[0] {
+		case "add":
+			if CheckTwoStrings(tokens) {
+				AddClient(tokens[1])
+			}
+		case "rm":
+			if CheckTwoStrings(tokens) {
+				id, err := strconv.Atoi(tokens[1])
+				if err != nil {
+					fmt.Println("Not an ID.")
+					continue
+				}
+				RemoveClient(id)
+			}
+		case "removeallclients":
+			RemoveAllClients()
+		case "ls":
+			ShowClients()
+		case "kill":
+			print_title()
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid command: [", input, "]")
+		}
+	}
+}
